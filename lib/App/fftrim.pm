b0016df348fd07abf35e78cb2a32b5250a902509
Joel Roth <joelz@pobox.com>
2018-11-12 15:07:23 -1000

    bump version to 0.04

--- lib/App/fftrim.pm
+++ lib/App/fftrim.pm
@@ -283,11 +283,11 @@ B<fftrim> - concatenate, trim and compress video files
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
